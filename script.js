912a8a8f3ca4d458d5fc03e1be96c1e22c6c7080
Linh Nguyen <linhnhatmen@gmail.com>
2020-09-22 16:37:29 -0700

    Setup charsets

--- script.js
+++ script.js
@@ -35,11 +35,20 @@ generateBtn.addEventListener("click", writePassword);
 //////////////////////////////////////////////////////////////////////
 
 function generatePassword() {
+  var password = "";
   //Ask the user for the length of password;
   var length = passwordLength();
 
   //Ask the user for character types included
   var types = passwordTypes();
+
+  //Store chosen charset in this array
+  var typeArray = [];
+
+  var lowerSet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperSet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var numericSet = ["0","1","2","3","4","5","6","7","8","9"];
+  var specialSet = ["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
 }
 
 //Check the password's length
